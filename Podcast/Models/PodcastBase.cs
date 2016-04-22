1e5f125d71cc06a2d6ba9364bce4bd06e163e725
Paulo Rubens Rodrigues Aguiar de Lima <paulorubenslima@gmail.com>
2016-04-22 16:26:45 -0300

    Alterado o tipo de dado da data de gravação de String para DateTime

--- Podcast/Models/PodcastBase.cs
+++ Podcast/Models/PodcastBase.cs
@@ -10,7 +10,7 @@ namespace Podcast.Models
         public int PodcastBaseID { get; set; }
         public string dsTitulo { get; set; }
         public string dsPodcast { get; set; }
-        public string dtGravacao { get; set; }
+        public DateTime dtGravacao { get; set; }
         public int nrEdicao { get; set; }
         public string nmArquivoAudio { get; set; }
         public string nmArquivoImagem { get; set; }
